faface5fb6f288e5b03c5c8011257f13f149d15c
henrybasu <hbasu@macalester.edu>
2021-12-02 17:30:26 -0600

    fixed bug in mutate

--- ALifeSimTest/ALifeSimTest.py
+++ ALifeSimTest/ALifeSimTest.py
@@ -556,7 +556,13 @@ class ALifeSimTest(object):
         elif randElem == 5:
             newVal = random.choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-        newBabyGeneticString[randElem] = newVal
+        babyGeneticStringAsList = list(newBabyGeneticString)
+        print(babyGeneticStringAsList)
+        babyGeneticStringAsList[randElem] = newVal
+        newBabyGeneticString = ""
+        for j in range(len(babyGeneticStringAsList)):
+            newBabyGeneticString+=str(babyGeneticStringAsList[j])
+        print(newBabyGeneticString)
         return newBabyGeneticString
 
 
